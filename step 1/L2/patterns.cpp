983ae4226116d3fa4b267a8e09fe5ee0a5b53c65
aryan <aryanthawakar975@gmail.com>
2025-10-16 18:26:22 +0530

    pattern 17 completed

--- step 1/L2/patterns.cpp
+++ step 1/L2/patterns.cpp
@@ -166,9 +166,14 @@ void pattern17(int n ){
         for(int j = 0 ; j < n-i-1 ; j++){
             cout<<" ";
         }
-        for(int k = 0; k < i*2+1 ; k++ ){
-            cout<<"*";
+        char ch = 'A';
+        int breakpoint = (2*i+1) /2;
+        for(int k = 1; k <= i*2+1 ; k++ ){
+            cout<<ch;
+            if(k <= breakpoint) ch++;
+            else ch--;
         }
+
         for(int j = 0 ; j < n-i-1 ; j++){
             cout<<" ";
         }
