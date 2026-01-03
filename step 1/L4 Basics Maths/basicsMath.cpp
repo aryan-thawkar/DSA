e075552ec7a5d234e301011a519c536b2763da9c
aryan <aryanthawakar975@gmail.com>
2026-01-03 17:19:00 +0530

    basics maths : reverse a number

--- step 1/L4 Basics Maths/basicsMath.cpp
+++ step 1/L4 Basics Maths/basicsMath.cpp
@@ -10,8 +10,27 @@ int countDigits(int n){
     return digits;
 }
 
+int revNumber(int x){
+    int rem = 0 ;
+        long int rev = 0;
+        bool isNegative = x < 0;
+        if(isNegative){
+            x = x * (-1);
+        }
+        while(x > 0){
+            rem = x % 10;
+            rev = (rev * 10) + rem;
+            x = x / 10;
+        }
+        if(isNegative){
+            rev = -rev;
+        }
+        return rev;
+}
+
 int main(){
 
-    cout<<countDigits(123456789);
+    cout<<countDigits(123456789)<<endl;
+    cout<<revNumber(1534236469)<<endl;
     return 0;
 }
