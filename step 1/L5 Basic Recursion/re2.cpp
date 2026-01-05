ec9684cbd6bbf3c6e896d5a8b54779e015b3d1ea
aryan <aryanthawakar975@gmail.com>
2026-01-05 20:02:52 +0530

    basics recursion : print 1 to N using recursion

--- step 1/L5 Basic Recursion/re2.cpp
+++ step 1/L5 Basic Recursion/re2.cpp
@@ -7,7 +7,14 @@ void printName(int i , int n){
     printName(i+1 , n);
 }
 
+void print1toN(int i , int n){
+    if(i>n) return;
+    cout<<i<<" ";
+    print1toN(i+1 , n);
+}
+
 int main(){
     printName(1 , 5);
+    print1toN(1,10);
     return 0;
 }
