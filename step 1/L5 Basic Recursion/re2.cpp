17f0a2bdb298ddd9ef5751be74bbd5bba95ea4c5
aryan <aryanthawakar975@gmail.com>
2026-01-05 19:59:21 +0530

    basics recursion : print names using recursion

---
+++ step 1/L5 Basic Recursion/re2.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void printName(int i , int n){
+    if(i>n) return;
+    cout<<"Aryan "<<endl;
+    printName(i+1 , n);
+}
+
+int main(){
+    printName(1 , 5);
+    return 0;
+}
