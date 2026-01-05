aea1f08d65bf21a3b78b07021359675100f5e0b8
aryan <aryanthawakar975@gmail.com>
2026-01-05 20:18:40 +0530

    basics recursion : print N to 1  and  1 to N using backtrack

--- step 1/L5 Basic Recursion/re2.cpp
+++ step 1/L5 Basic Recursion/re2.cpp
@@ -19,10 +19,26 @@ void printNto1(int i , int n){
     printNto1(i-1 , n);
 }
 
+void print1toNBacktrach(int i , int n){
+    if(i<1) return;
+    print1toNBacktrach(i-1 , n);
+    cout<<i<<" ";
+}
+
+void printNto1Backtrack(int i , int n){
+    if(i>n) return;
+    printNto1Backtrack(i+1 , n);
+    cout<<i<<" ";
+}
+
 int main(){
     printName(1 , 5);
     print1toN(1,10);
     cout<<endl;
     printNto1(10,10);
+    cout<<endl;
+    print1toNBacktrach(10,10);
+    cout<<endl;
+    printNto1Backtrack(1,10);
     return 0;
 }
